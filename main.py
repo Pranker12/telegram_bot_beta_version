39db0f1c9a5ae389a6855d6f5fc9d5601def6aeb
Pranker12 <166895775+Pranker12@users.noreply.github.com>
2024-08-31 23:24:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import os
 import sys
 
 from bot.bot import dp, bot
@@ -7,6 +8,8 @@ from database.models import async_main
 
 
 async def main() -> None:
+    if not os.path.exists('db'):
+        os.mkdir('db')
     await async_main()
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
